e95dc20589433d8dcabcabe53a8c09e946409c04
Cristopher Andrade <133789100+cristopheroan@users.noreply.github.com>
2024-05-16 15:46:36 +0000

    For

--- Estrutura-Repetição/src/ExemploFor.java
+++ Estrutura-Repetição/src/ExemploFor.java
@@ -1,5 +1,10 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class ExemploFor {
+
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos ++ ){
+            System.out.println("Contando Carneirinhos" + carneirinhos);
+        }
+        System.out.println("Joãozinho Dormiu.");
     }
 }
+
